3502eed6e182dc93ef5affa99ff479c4f4d4b26c
Krinzh <23oleg03@gmail.com>
2025-05-16 04:43:57 +0200

    f

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const images = [
   {
     preview:
@@ -64,39 +66,41 @@ const images = [
   },
 ];
 
-const galleryContainer = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
+let listHTML = '';
+images.forEach(
+  ({ preview, original, description }) =>
+    (listHTML += `<li class="gallery-item">
+  <a class="gallery-link" href="${original}">
+    <img
+      class="gallery-image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`)
+);
+gallery.insertAdjacentHTML('afterbegin', listHTML);
 
-function createGalleryItems(images) {
-  return images
-    .map(({ preview, original, description }) => {
-      return `
-    <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img
-          class="gallery-image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>`;
-    })
-    .join('');
-}
+gallery.addEventListener('click', selectIMG);
 
-galleryContainer.innerHTML = createGalleryItems(images);
-
-galleryContainer.addEventListener('click', (event) => {
+function selectIMG(event) {
   event.preventDefault();
 
-  const isImage = event.target.classList.contains('gallery-image');
-  if (!isImage) return;
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
 
-  const largeImageURL = event.target.dataset.source;
+  const selectedImgSrc = event.target.dataset.source;
 
-  const instance = basicLightbox.create(`
-    <img src="${largeImageURL}" width="800" height="600">
-  `);
+  const lightBox = basicLightbox.create(`<img src="${selectedImgSrc}">`, {
 
-  instance.show();
-});
+    onShow: lightBox => {
+      lightBox.element().querySelector('img').onclick = lightBox.close;
+    },
+    closable: true,
+  });
+
+  lightBox.show();
+}
